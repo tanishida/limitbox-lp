ea9b0500db132c1f9d4c22bdc21efbb01b060ede
tanishida <tanishi.lovelive@gmail.com>
2026-07-10 16:58:52 +0900

    fix redirect

--- vite.config.js
+++ vite.config.js
@@ -69,6 +69,10 @@ export default defineConfig({
           __dirname,
           'appStoreScreenshotCustomizer.html',
         ),
+        appStoreScreenshotSplitter: resolve(
+          __dirname,
+          'appStoreScreenshotSplitter.html',
+        ),
       },
     },
   },
